0c6c498f97c2a5de727386cfd3905063b5cef786
linhnguyen1001 <nguyenminhlinh1417@gmail.com>
2020-06-16 00:57:21 +0700

    Add project files.

---
+++ Lab7API/Controllers/SachController.cs
@@ -0,0 +1,32 @@
+﻿using Lab7API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lab7API.Controllers
+{
+    public class SachController : ApiController
+    {
+        Sach[] sachs = new Sach[]
+        {
+            new Sach { Id = 1, Title = "Tôi thấy hoa vàng trên cỏ xanh", AuthorName = "Nguyễn Nhật Ánh", Price = 1, Content = "Truyện kể về tuổi thơ..."},
+            new Sach { Id = 2, Title = "Pro ASP.NET MVC5", AuthorName = "Adam Freeman", Price = 3.75M, Content = "The latest evolution of Microsoft's"},
+        };
+        public IEnumerable<Sach> GetAll()
+        {
+            return sachs;
+        }
+        public IHttpActionResult GetSach(int id)
+        {
+            var sach = sachs.FirstOrDefault((p) => p.Id == id);
+            if (sach == null)
+            {
+                return NotFound();
+            }
+            return Ok(sach);
+        }
+    }
+}
---
+++ Lab7API/Models/Sach.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab7API.Models
+{
+    public class Sach
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Content { get; set; }
+        public String AuthorName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
